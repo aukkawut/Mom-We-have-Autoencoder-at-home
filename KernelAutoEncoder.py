525686fcb4498edee682c7247c4377512da53fd5
Aukkawut Ammartayakun <50354662+aukkawut@users.noreply.github.com>
2023-11-21 08:52:24 -0500

    update visualization

--- KernelAutoEncoder.py
+++ KernelAutoEncoder.py
@@ -1,60 +1,49 @@
-#This is autoencoder from PCA and KR regression
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_openml
-from sklearn.linear_model import LinearRegression, ElasticNet,ARDRegression, Lasso, Ridge
+from sklearn.decomposition import KernelPCA
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.decomposition import PCA
+from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
-# Load data from openml
-mnist = fetch_openml('mnist_784', parser = 'auto', cache=True)
-X = mnist.data
-y = mnist.target
+# Load EMNIST data
+emnist = fetch_openml(name="Fashion-MNIST", parser='auto', cache = True)
+X, y = emnist.data / 255.0, emnist.target.astype(int)
 
-# Split data into train and test
-X_train, X_test, _, _ = train_test_split(X, y, test_size=0.1, random_state=42)
+# Flatten images
+X_flat = X.values
 
-# PCA
-pca = PCA(n_components=32)
-pca.fit(X_train)
-X_train_pca = pca.transform(X_train)
-X_test_pca = pca.transform(X_test)
+# Split data into training and testing sets
+X_train, X_test, _, _ = train_test_split(X_flat, y, test_size=0.5, random_state=42)
 
-# Kernel regression on PCA transformed data to get the reconstruction of the intial MNIST image
-kr = KernelRidge(alpha=0.3, kernel='sigmoid')
-kr.fit(X_train_pca, X_train)
+# Apply Kernel PCA for non-linear dimensionality reduction
+kpca = KernelPCA(n_components=16, kernel='rbf', gamma=0.01)
+X_train_kpca = kpca.fit_transform(X_train)
+X_test_kpca = kpca.transform(X_test)
 
-# Randomly pick 10 from test set to show the initial image, PCA transformed image and the reconstructed image
-initial_image = X_test.sample(n=10, random_state=42)
-initial_image = np.array(initial_image)
-pca_image = pca.transform(initial_image)
-reconstructed_image = kr.predict(pca_image)
+# Train Kernelized Regression Model for reconstruction
+krr = KernelRidge(alpha=1e-6, kernel='rbf', gamma=0.01)
+krr.fit(X_train_kpca, X_train)
 
-# Plot the images
-plt.figure(figsize=(20, 4))
-for i in range(10):
-    # initial image
-    ax = plt.subplot(3, 20, i+1)
-    plt.imshow(initial_image[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    
-    # PCA transformed image
-    ax = plt.subplot(3, 20, i+1+20)
-    plt.imshow(pca_image[i].reshape(4, 8))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    
-    # Reconstructed image
-    ax = plt.subplot(3, 20, i+1+40)
-    plt.imshow(reconstructed_image[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.tight_layout()
+# Reconstruct images
+reconstructed_images = krr.predict(X_test_kpca)
+
+# Calculate reconstruction error
+mse = mean_squared_error(X_test, reconstructed_images)
+print(f"Mean Squared Error: {mse}")
+
+# Visualize original and reconstructed images
+n_samples = 5
+plt.figure(figsize=(10, 4))
+for i in range(n_samples):
+    plt.subplot(2, n_samples, i + 1)
+    plt.imshow(X_test[i].reshape(28, 28), cmap='gray')
+    plt.title("Original")
+    plt.axis('off')
+
+    plt.subplot(2, n_samples, i + 1 + n_samples)
+    plt.imshow(reconstructed_images[i].reshape(28, 28), cmap='gray')
+    plt.title("Reconstructed")
+    plt.axis('off')
+
 plt.show()
-
-
